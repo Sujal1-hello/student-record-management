658f09b69bd439e75940b89e8eeee629b8c69c72
Sujal1-hello <sujalrai395@gmail.com>
2026-08-06 17:18:43 +0545

    Initial Laravel setup with student migration

---
+++ database/migrations/2026_08_06_111804_create_students_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+ public function up(): void
+{
+    Schema::create('students', function (Blueprint $table) {
+        $table->id();
+        $table->string('student_id')->unique();
+        $table->string('first_name');
+        $table->string('last_name');
+        $table->string('email')->unique();
+        $table->string('phone')->nullable();
+        $table->date('date_of_birth')->nullable();
+        $table->enum('gender', ['Male', 'Female', 'Other']);
+        $table->string('course');
+        $table->integer('semester');
+        $table->timestamps();
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
